4cc88320d9ef175e786c6ec17d779ed2108b436f
Bharath Srinivas <cybermaster192@gmail.com>
2018-01-07 03:32:24 +0530

    refactored: use a version constant

--- cmd/aws-go/version.go
+++ cmd/aws-go/version.go
@@ -6,14 +6,16 @@ import (
 	"github.com/spf13/cobra"
 )
 
+const version = "v0.2.0"
+
 var versionCmd = &cobra.Command{
 	Use: "version",
 	Short: "Prints the version information and exit",
 	Args: cobra.NoArgs,
 	Run: func(cmd *cobra.Command, args []string) {
 		fmt.Println("aws-go:")
-		fmt.Println(" version 	: v0.1.0")
-		fmt.Println(" build date	: 2017-12-31")
+		fmt.Println(" version 	:", version)
+		fmt.Println(" build date	: 2018-01-07")
 		fmt.Println(" go version	: go1.9.2")
 		fmt.Println(" platform	: linux/amd64")
 	},
